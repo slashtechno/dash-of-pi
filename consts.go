01d10677a55b4207521472c2eff8912f683f5f50
slashtechno <77907286+slashtechno@users.noreply.github.com>
2025-12-21 18:08:03 -0600

    fix: add default rotation value for upside-down camera mount

--- consts.go
+++ consts.go
@@ -75,6 +75,7 @@ const (
 	DefaultVideoFPS       = 30
 	DefaultVideoWidth     = 1920
 	DefaultVideoHeight    = 1080
+	DefaultRotation       = 270  // Rotate by 270 degrees since the design I created mounts the camera upside-down and I'll probably end up including the STL
 	DefaultSegmentLengthS = 60   // seconds
 	DefaultMJPEGQuality   = 8    // 2-31 scale, lower is better, 8=good balance
 	DefaultEmbedTimestamp = true // Embed timestamp by default
